565394e16aaeebd81907c8443c5692d3661a0aaf
이진형 <jhlee@bigsolco.kr>
2022-08-02 17:34:07 +0900

    first init

---
+++ src/main/java/com/board/board/service/BoardService.java
@@ -0,0 +1,58 @@
+package com.board.board.service;
+
+import com.board.board.entity.Board;
+import com.board.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository; //DI를 통해서 주입해준다.
+
+    //글 작성 처리
+    public void write(Board board, MultipartFile file) throws Exception{
+
+        String projectPath = System.getProperty("user.dir") + "/src/main/resources/static/files";
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/"+fileName);
+
+        boardRepository.save(board);
+    } //save : entity를 넣어준다.
+
+    //게시글 리스트 처리
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
+    //특정 게시글 불러오기
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id) {
+
+        boardRepository.deleteById(id);
+    }
+}
+
+
+
+//service는 controller에서 사용한다.
